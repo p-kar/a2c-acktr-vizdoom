e891759e2cf556f2ccf512fe5c3c82d78782635e
Pratyush Kar <pratyush.kar@gmail.com>
2017-11-22 20:04:05 -0600

    added enjoy to visualize agent

--- enjoy.py
+++ enjoy.py
@@ -4,24 +4,26 @@ import os
 import torch
 from torch.autograd import Variable
 
-from envs import make_env
+from envs import make_visual_env
+from vec_env import VecEnv
+from time import sleep
 
 
 parser = argparse.ArgumentParser(description='RL')
 parser.add_argument('--algo', default='a2c',
-                    help='algorithm to use: a2c | ppo | acktr')
+                    help='algorithm to use: a2c | acktr')
 parser.add_argument('--seed', type=int, default=1,
                     help='random seed (default: 1)')
 parser.add_argument('--num-stack', type=int, default=4,
                     help='number of frames to stack (default: 4)')
 parser.add_argument('--log-interval', type=int, default=10,
                     help='log interval, one log per n updates (default: 10)')
-parser.add_argument('--env-name', default='PongNoFrameskip-v4',
-                    help='environment to train on (default: PongNoFrameskip-v4)')
+parser.add_argument('--env-name', default='VizDoom',
+                    help='environment to train on (default: VizDoom)')
 parser.add_argument('--load-dir', default='./trained_models/',
                     help='directory to save agent logs (default: ./trained_models/)')
-parser.add_argument('--log-dir', default='/tmp/gym/',
-                    help='directory to save agent logs (default: /tmp/gym)')
+parser.add_argument('--log-dir', default='/tmp/doom/',
+                    help='directory to save agent logs (default: /tmp/doom)')
 args = parser.parse_args()
 
 try:
@@ -29,54 +31,37 @@ try:
 except OSError:
     pass
 
-env = make_env(args.env_name, args.seed, 0, args.log_dir)()
+envs = VecEnv([make_visual_env()])
 
 actor_critic = torch.load(os.path.join(args.load_dir, args.env_name + ".pt"))
 actor_critic.eval()
 
-obs_shape = env.observation_space.shape
+obs_shape = envs.observation_space_shape
 obs_shape = (obs_shape[0] * args.num_stack, *obs_shape[1:])
 current_obs = torch.zeros(1, *obs_shape)
 
 
 def update_current_obs(obs):
-    shape_dim0 = env.observation_space.shape[0]
+    shape_dim0 = envs.observation_space_shape[0]
     obs = torch.from_numpy(obs).float()
     if args.num_stack > 1:
         current_obs[:, :-shape_dim0] = current_obs[:, shape_dim0:]
     current_obs[:, -shape_dim0:] = obs
 
-env.render('human')
-obs = env.reset()
+obs = envs.reset()
 update_current_obs(obs)
 
-if args.env_name.find('Bullet') > -1:
-    import pybullet as p
-
-    torsoId = -1
-    for i in range(p.getNumBodies()):
-        if (p.getBodyInfo(i)[0].decode() == "torso"):
-            torsoId = i
-
 while True:
+    sleep(0.01)
     value, action = actor_critic.act(Variable(current_obs, volatile=True),
                                         deterministic=True)
     cpu_actions = action.data.cpu().numpy()
 
     # Obser reward and next obs
-    obs, _, done, _ = env.step(cpu_actions[0])
-
-    if args.env_name.find('Bullet') > -1:
-        if torsoId > -1:
-            distance = 5
-            yaw = 0
-            humanPos, humanOrn = p.getBasePositionAndOrientation(torsoId)
-            p.resetDebugVisualizerCamera(distance, yaw, -20, humanPos)
-
-    env.render('human')
+    obs, _, done, _ = envs.step([cpu_actions[0]])
 
     if done:
-        obs = env.reset()
+        obs = envs.reset()
         actor_critic = torch.load(os.path.join(args.load_dir, args.env_name + ".pt"))
         actor_critic.eval()
 
