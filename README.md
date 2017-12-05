8d98282bd10ec04369b43a5f63f02974cfd89dd7
Venkata Sailesh Sanampudi <vsailesh@cs.utexas.edu>
2017-12-05 16:24:28 -0600

    Minor change to driver.py

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # a2c-acktr-vizdoom
 A2C and ACKTR implementations for ViZDoom
 
+# Packages to install
+pytorch
+scipy
+sdl2
+vizdoom
+
+
 ## Training the agent
 ~~~~
 python main.py --algo a2c --num-processes 16 --config-path scenario/health_gathering.cfg --num-frames 10000000 --log-dir $LOG_DIR --save-dir $SAVE_DIR --no-vis
--- driver.py
+++ driver.py
@@ -2,10 +2,18 @@ from envs import make_visual_env, make_env
 from vec_env import VecEnv
 from time import sleep
 from random import choice
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--vis', type=int, default=0)
+args = parser.parse_args()
 num_envs = 1
 
-envs = VecEnv([make_visual_env('./scenarios/deathmatch_maze.cfg') for i in range(num_envs)])
+if args.vis:
+    envs = VecEnv([make_visual_env('./scenarios/deathmatch_maze.cfg') for i in range(num_envs)])
+else:
+    envs = VecEnv([make_env(0, './scenarios/deathmatch_maze.cfg') for i in range(num_envs)])
+
 
 # Define some actions. Each list entry corresponds to declared buttons:
 # MOVE_LEFT, MOVE_RIGHT, ATTACK
